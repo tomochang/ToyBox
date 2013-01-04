90d1e44c4d0fd813e2311b05cf0ff61800fa9ac6
Tomonori Mizuno <tomonori.mizuno@uzabase.com>
2013-01-05 03:58:36 +0900

    サーバのディレクトリ容量が指定値を超えていた場合にエラーで落とす。Hinemosと絡めて運用。zabbix早く入れたい・・・

---
+++ shell/check_aleart_df.sh
@@ -0,0 +1,13 @@
+R_USEAGE=`df -h | egrep '(9[5-9]%|100%)'|wc -l`
+WORK_DATE="`date +%Y%m%d`"
+ADDRESS="hogehoge@hogehoge.com"
+MAIL_TITLE="`hostname`で容量が95%を超えているディレクトリが有ります。至急確認して下さい"
+MESSAGE_FILE="/your_directory/CHECK_DF_ALEART_${WORK_DATE}"
+
+echo $WORK_DATE
+df -h | egrep '(9[5-9]%|100%)'>$MESSAGE_FILE
+
+if [ $DIR_USEAGE -ne 0 ]
+then
+  exit 1
+fi
